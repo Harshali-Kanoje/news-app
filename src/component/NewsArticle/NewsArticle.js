f967044c145d1a5c023d6006b63ceba314021a33
Harshali Kanoje <harshalikanoje@gmail.com>
2023-10-16 17:08:50 -0700

    more details are added

--- src/component/NewsArticle/NewsArticle.js
+++ src/component/NewsArticle/NewsArticle.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import './NewsArticle.css'
 
-const NewsArticle = ({title , description , urlToImage ,publishedAt , url , index}) => {
+const NewsArticle = ({ title, description, urlToImage, publishedAt, url, index, author }) => {
   return (
     <div key={index}>
-       <div className='newsdetails' >
-                     <img src={urlToImage}/>
-                     <p>{title}</p>
+      <div className='newsdetails' >
+        <img src={urlToImage} />
+        <h1>{title}</h1>
+        <div className='article-details'>
+          <span className='author'>{author}</span>
+          <span className='author'>{publishedAt}</span>
         </div>
+        <p>{description}</p>
+        <a href={url} target='_blank'>Read More</a>
+      </div>
     </div>
   )
 }
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -5,42 +5,58 @@ import NewsArticle from '../../component/NewsArticle/NewsArticle';
 
 const Home = () => {
 
-    const [news , setNews] = useState([]);
-    const [searchnews , setSearchnews] = useState('pune');
-    
-        const loadNews = async () => {
-            try{
-            const response = await axios.get(`https://newsapi.org/v2/everything?q=${searchnews}&from=2023-10-05&to=2023-10-05&sortBy=popularity&apiKey=${process.env.REACT_APP_API_KEY}`)
-            setNews(response.data.articles);
-            }
-            catch(e) {
-            console.log(e)
-            }
-        }
-       
+  const [news, setNews] = useState([]);
+  const [searchnews, setSearchnews] = useState('Nagpur');
 
-    useEffect(() => {
-        loadNews()
-    },[])
+  const loadNews = async () => {
 
-    useEffect(() => {
-        loadNews()
-    },[searchnews])
+    try {
+      const response = await axios.get(`https://newsapi.org/v2/everything?q=${searchnews}&from=2023-10-05&to=2023-10-05&sortBy=popularity&apiKey=${process.env.REACT_APP_API_KEY}`)
+      setNews(response.data.articles);
+      console.log(response.data.articles)
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
+
+
+  useEffect(() => {
+    loadNews()
+
+  }, [])
+
+  useEffect(() => {
+    loadNews()
+  }, [searchnews])
 
   return (
     <div>
-        <input type='text' value={searchnews} onChange={(e) => {
+      <div className='news-head'>
+        <h1> Daily News</h1>
+        <input type='text'
+          value={searchnews}
+          onChange={(e) => {
             setSearchnews(e.target.value)
-        }}/>
-      {
-        news.map((newsdata , index) => {
-            const {title , description , urlToImage ,publishedAt , url} = newsdata;
-            return(
-               <NewsArticle title={title} description={description} url={url} urlToImage={urlToImage} publishedAt={publishedAt} index={index}/>
+          }}
+          placeholder='Search Your News' />
+      </div>
+
+      <h1 className='text-center'>Top Search News Are Here : {searchnews}</h1>
+      {/* <li onClick={handlesearch('india')}>Home</li> */}
+
+      <div className='news-container'>
+        {
+          news.map((newsdata, index) => {
+            const { title, description, urlToImage, publishedAt, url, author } = newsdata;
+            return (
+              <NewsArticle title={title} description={description} url={url} urlToImage={urlToImage} publishedAt={publishedAt} index={index} author={author} />
             )
-            
-        })
-      }
+
+          })
+        }
+      </div>
+
     </div>
   )
 }
